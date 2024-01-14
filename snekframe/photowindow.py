027f0a00f2a662d300f270d4f119dff8652550b7
Matt Abdul-Rahim <Matthewar@users.noreply.github.com>
2024-01-14 22:17:34 +0000

    Issue #49: Add missing class.

--- snekframe/photowindow.py
+++ snekframe/photowindow.py
@@ -124,7 +124,7 @@ class PhotoTitleBar:
             open_voltage_warning()
             self._title.text = "Low Voltage Warning"
 
-        self._voltage_button = self._title_menu_buttons.add_button(title_menu, callback_open_voltage, selected=False, enabled=False, show_warning_callback=self._show_voltage_warning, hide_warning_callback=self._hide_voltage_warning, close_warning_callback=callback_open_settings)
+        self._voltage_button = self._title_menu_buttons.add_button(title_menu, callback_open_voltage, button_cls=_VoltageWarningIconRadioButton, selected=False, enabled=False, show_warning_callback=self._show_voltage_warning, hide_warning_callback=self._hide_voltage_warning, close_warning_callback=callback_open_settings)
         # Setup grid options
         self._voltage_button.grid(row=0, column=column, padx=5)
         self._voltage_button.grid_remove()
